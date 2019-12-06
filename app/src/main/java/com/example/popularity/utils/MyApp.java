08fb056aeb957f017565aa06bab25e7e6c94d51d
zahramsv <zmosavi01@gmail.com>
2019-12-06 18:04:15 +0330

    merge branches

--- app/src/main/java/com/example/popularity/utils/MyApp.java
+++ app/src/main/java/com/example/popularity/utils/MyApp.java
@@ -3,6 +3,8 @@ package com.example.popularity.utils;
 import android.app.Application;
 
 import com.example.popularity.R;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
@@ -11,6 +13,8 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath("fonts/Samim_FD.ttf").build());
 
     }
